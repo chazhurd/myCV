9da60dc3129ae294070f6614e3225056bfffd275
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2021-01-19 00:09:08 -0800

    added spotDev
    spot site and description with link. Will have to use youtube for video.

---
+++ SpotScripts.js
@@ -0,0 +1,113 @@
+var timed = 0;
+var x = 30;
+var y = 17;
+var a = 37;
+var b = 17;
+var c = 43;
+var d = 16;
+
+
+function main() {
+    drawLogo();
+    siteAnimate();
+    var randomBlink = setInterval(() => {
+        var allowBlink = Math.random();
+        if (allowBlink < .5) {
+            var blinkTl = new TimelineMax();
+            var topText = document.getElementById("topNeon");
+            var bottomText = document.getElementById("bottomNeon");
+            blinkTl.fromTo(topText, 0.1, { opacity: "1" }, { opacity: "0.2" })
+                .fromTo(topText, 0.1, { opacity: "0.2" }, { opacity: "1" })
+                .fromTo(bottomText, 0.1, { opacity: "1" }, { opacity: "0.2" })
+                .fromTo(bottomText, 0.3, { opacity: "0.2" }, { opacity: "1" });
+        }
+    }, 4000);
+}
+
+function siteAnimate() {
+    var openTl = new TimelineMax();
+    var topText = document.getElementById("topNeon");
+    var bottomText = document.getElementById("bottomNeon");
+    openTl.fromTo(topText, 0.1, { opacity: "0" }, { opacity: ".3" })
+        .fromTo(topText, 0.1, { opacity: ".3" }, { opacity: "0" })
+        .fromTo(topText, 0.1, { opacity: "0" }, { opacity: ".5" })
+        .fromTo(topText, 0.1, { opacity: ".5" }, { opacity: "0" })
+        .fromTo(topText, 1, { opacity: "0" }, { opacity: "1" })
+        .fromTo(bottomText, 0.2, { opacity: "0.6" }, { opacity: "0" })
+        .fromTo(bottomText, 0.1, { opacity: "0" }, { opacity: "1" })
+        .fromTo(bottomText, 0.1, { opacity: "1" }, { opacity: "1" })
+        .fromTo(bottomText, 1, { opacity: "0" }, { opacity: "1" });
+}
+//Banner
+function drawLogo() {
+    var can = document.getElementById("myCanvas2");
+    var cantx = can.getContext("2d");
+    cantx.lineWidth = 1;
+    cantx.strokeStyle = 'white';
+
+    var myTimer = setInterval(function() {
+        if (timed < 1) {
+            cantx.fillStyle = '#333';
+            cantx.fillRect(0, 0, 60, 40);
+            cantx.beginPath();
+        }
+        if (timed < 5) {
+            cantx.moveTo(x, y);
+            x -= 0.8;
+            y -= 1.4;
+            cantx.lineTo(x, y);
+            cantx.stroke();
+            timed++;
+        }
+        if (timed >= 5 && timed <= 15) {
+            cantx.moveTo(x, y);
+            x -= 0.6;
+            y += 2;
+            cantx.lineTo(x, y);
+            cantx.stroke();
+            timed++;
+        }
+        if (timed > 15 && timed <= 30) {
+            cantx.moveTo(x, y);
+            x += 2.2;
+            y -= .9;
+            cantx.lineTo(x, y);
+            cantx.stroke();
+            timed++;
+        }
+
+        if (timed > 30 && timed <= 37) {
+            cantx.moveTo(a, b);
+            a += 0;
+            b += 2.5;
+            cantx.lineTo(a, b);
+            cantx.stroke();
+            timed++;
+        }
+        if (timed > 37 && timed <= 44) {
+            cantx.moveTo(c, d);
+            c += 0;
+            d += 2.5;
+            cantx.lineTo(c, d);
+            cantx.stroke();
+            timed++;
+
+        }
+        if (timed >= 44 && timed <= 55) {
+            timed++;
+        }
+        if (timed >= 55) {
+            timed++;
+            if (timed >= 65) {
+                x = 30;
+                y = 17;
+                a = 37;
+                b = 17;
+                c = 43;
+                d = 16;
+                timed = 0;
+            }
+        }
+    }, 34);
+}
+//Banner above
