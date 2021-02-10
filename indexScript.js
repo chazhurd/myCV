8881ec7bc0422cbef0ff295d63c5603a480c00ed
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2021-02-09 17:44:50 -0800

    Phone Update
    Window Check made here. Phones werent reacting to bounds like desktops.

--- indexScript.js
+++ indexScript.js
@@ -9,31 +9,33 @@ var checkViewPort = setInterval(() => {
             var pIndex = elementLeft.indexOf("p");
             elementLeft = parseInt(elementLeft.substr(0, pIndex));
 
-
+            var windowWidth = parseInt(screen.width);
             var bounding = myElement.getBoundingClientRect();
             var myElementHeight = myElement.offsetHeight;
-            var myElementWidth = myElement.offsetWidth;
-
-            if (bounding.top >= -myElementHeight + 150 &&
-                bounding.bottom + 150 <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
-                inside = true;
-            } else {
-                inside = false;
-            }
-
-            if (inside) {
-                divTl = new TimelineMax();
-                if (!placed) {
-                    divTl.fromTo(myElement, .2, { left: elementLeft }, { left: "0px" });
-                    placed = true;
+            if (windowWidth > 700) {
+                if (bounding.top >= -myElementHeight + 150 &&
+                    bounding.bottom + 150 <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
+                    inside = true;
+                } else {
+                    inside = false;
                 }
-                console.log("Were In");
-            } else {
-                if (placed) {
-                    divTl.fromTo(myElement, .2, { left: elementLeft }, { left: "-450px" });
-                    placed = false;
+
+                if (inside) {
+                    divTl = new TimelineMax();
+                    if (!placed) {
+                        divTl.fromTo(myElement, .2, { left: elementLeft }, { left: "0px" });
+                        placed = true;
+                    }
+                    console.log("Were In");
+                } else {
+                    if (placed) {
+                        divTl.fromTo(myElement, .2, { left: elementLeft }, { left: "-450px" });
+                        placed = false;
+                    }
+                    console.log("Were Out");
                 }
-                console.log("Were Out");
+            } else {
+                myElement.style.left = "0px";
             }
         } catch {
 
