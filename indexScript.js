d77e2458b3ed5cfb16ad8a9d517cb88eed53cee0
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2021-02-12 17:37:36 -0800

    GSAP
    GSAP update, all are timed right now. Added some animation to the CV for each bar and changed direction of advert.

--- indexScript.js
+++ indexScript.js
@@ -8,6 +8,11 @@ var a = 37;
 var b = 17;
 var c = 43;
 var d = 16;
+jTrig = false;
+cTrig = false;
+sTrig = false;
+hTrig = false;
+lTrig = false;
 
 
 function loadJS() {
@@ -27,6 +32,14 @@ function viewportAnimations() {
                     var windowWidth = parseInt(screen.width);
                     var bounding = myElement.getBoundingClientRect();
                     var myElementHeight = myElement.offsetHeight;
+
+                    var javaExpBar = document.getElementById("javaExp");
+                    var counter = 0;
+                    var javaExpBarHeight = javaExpBar.offsetHeight;
+                    var boundingJava = javaExpBar.getBoundingClientRect();
+
+
+                    /*new game animation*/
                     if (windowWidth > 700) {
                         if (bounding.top >= -myElementHeight + 150 &&
                             bounding.bottom + 150 <= (window.innerHeight || document.documentElement.clientHeight) + myElementHeight) {
@@ -38,12 +51,12 @@ function viewportAnimations() {
                         if (inside) {
                             divTl = new TimelineMax();
                             if (!placed) {
-                                divTl.fromTo(myElement, .7, { left: elementLeft }, { left: "0px" });
+                                divTl.fromTo(myElement, .7, { left: elementLeft, opacity: "0" }, { left: "0px", opacity: ".9" });
                                 placed = true;
                             }
                         } else {
                             if (placed) {
-                                divTl.fromTo(myElement, .7, { left: elementLeft }, { left: "-450px" });
+                                divTl.fromTo(myElement, .7, { left: elementLeft, opacity: ".9" }, { left: "450px", opacity: "0" });
                                 placed = false;
                             }
                         }
@@ -52,6 +65,76 @@ function viewportAnimations() {
                         myElement.style.left = "0px";
                     }
                 }
+                /* end new game animation*/
+
+                divT = new TimelineMax();
+                //java aniation
+                var javaExpBar = document.getElementById("javaExp");
+                var javaExpBarHeight = javaExpBar.offsetHeight;
+                var boundingJava = javaExpBar.getBoundingClientRect();
+                if (boundingJava.top >= -javaExpBarHeight + 50 &&
+                    boundingJava.bottom + 50 <= (window.innerHeight || document.documentElement.clientHeight) + javaExpBarHeight) {
+                    if (!jTrig) {
+                        jTrig = true;
+                        divT.fromTo(javaExpBar, 1, { width: "0%" }, { width: "90%" }, "-=1");
+                    }
+                }
+                //c animation
+                var cExpBar = document.getElementById("cExp");
+                var cExpBarHeight = cExpBar.offsetHeight;
+                var boundingC = cExpBar.getBoundingClientRect();
+
+                if (boundingC.top >= -cExpBarHeight + 50 &&
+                    boundingC.bottom + 50 <= (window.innerHeight || document.documentElement.clientHeight) + cExpBarHeight) {
+                    if (!cTrig) {
+                        cTrig = true;
+                        divT.fromTo(cExpBar, 1, { width: "0%" }, { width: "55%" }, "-=1");
+                    }
+                }
+
+                //sql animation
+                var sExpBar = document.getElementById("sExp");
+                var sExpBarHeight = cExpBar.offsetHeight;
+                var boundingS = sExpBar.getBoundingClientRect();
+
+                if (boundingS.top >= -sExpBarHeight + 50 &&
+                    boundingS.bottom + 50 <= (window.innerHeight || document.documentElement.clientHeight) + sExpBarHeight) {
+                    if (!sTrig) {
+                        sTrig = true;
+                        divT.fromTo(sExpBar, 1, { width: "0%" }, { width: "80%" }, "-=1");
+                    }
+                }
+
+                //html/css/js animation
+                var hExpBar = document.getElementById("hExp");
+                var hExpBarHeight = hExpBar.offsetHeight;
+                var boundingH = hExpBar.getBoundingClientRect();
+
+                if (boundingH.top >= -hExpBarHeight + 50 &&
+                    boundingH.bottom + 50 <= (window.innerHeight || document.documentElement.clientHeight) + hExpBarHeight) {
+                    if (!hTrig) {
+                        hTrig = true;
+                        divT.fromTo(hExpBar, 1, { width: "0%" }, { width: "70%" }, "-=1");
+                    }
+                }
+                //languages
+                var eExpBar = document.getElementById("eExp");
+                var eExpBarHeight = eExpBar.offsetHeight;
+                var boundingE = eExpBar.getBoundingClientRect();
+
+                var fExpBar = document.getElementById("fExp");
+                var spExpBar = document.getElementById("spExp");
+
+                if (boundingE.top >= -eExpBarHeight + 50 &&
+                    boundingE.bottom + 50 <= (window.innerHeight || document.documentElement.clientHeight) + eExpBarHeight) {
+                    if (!lTrig) {
+                        lTrig = true;
+                        divT.fromTo(eExpBar, 1, { width: "0%" }, { width: "98%" }, "-=1");
+                        divT.fromTo(fExpBar, 1, { width: "0%" }, { width: "55%" }, "-=1");
+                        divT.fromTo(spExpBar, 1, { width: "0%" }, { width: "25%" }, "-=1");
+                    }
+                }
+
             } catch {
                 var myElement = document.getElementById('animatedDiv');
                 myElement.style.left = "0px";
